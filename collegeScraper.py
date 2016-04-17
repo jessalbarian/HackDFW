3f1d96cc93b40df1b599cf2faa3bc657f61aad4e
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2016-04-17 09:34:55 -0500

    Adding physical addresses to school

--- collegeScraper.py
+++ collegeScraper.py
@@ -12,30 +12,33 @@ pageNumber = 1
 schoolListings = {}
 
 def testWorks():
-	while pageNumber < 297:
-		url = srcUrl + str(pageNumber)
-		request = requests.get(url)
-		data = request.text
-		soup = BeautifulSoup(data, "html.parser")
-		'''
-		for schoolSection in soup.findAll('div', {'class': 'school-listing'}):
-			schoolAddress = []
-			name =  schoolSection.find('p').text
-			for metaAddress in schoolSection.findAll('meta'):
-				schoolAddress.append(metaAddress['content'])
-			#webAddress = schoolSection.findAll('a', {'class': 'learn-more-school'})
-			#webAddress = schoolSection.find('a')
-		'''
-		pageNumber += 1
-		'''
-		for tag in soup.find('meta',{'class':'learn-more-school'}):
-			print tag['itemprop']
-		'''
-		metaStuff = soup.find('meta', {'class':'learn-more-school'})
-		for elementy in metaStuff(text=lambda text: isinstance(text, Comment)):
-			elementy.extract()
-			#print element
+        global schoolListings
+        global pageNumber
 
+        while pageNumber < 297:
+		try:
+			url = srcUrl + str(pageNumber)
+			request = requests.get(url)
+			data = request.text
+			soup = BeautifulSoup(data, "html.parser")
+			comments = soup.find_all(string=lambda text:isinstance(text, Comment))
+			for comment in comments:
+				if "http://schema.org/CollegeOrUniversity" in comment:
+					try:
+						comment = BeautifulSoup(str(comment), "html.parser")
+						name = comment.find('a').text.rstrip().lstrip()
+						schoolInfo = comment.find_all('meta')
+						try:
+							webpage = schoolInfo[4]['content']
+						except IndexError:
+							webpage = ""
+						schoolAddress = schoolInfo[0]['content'] + ", " + schoolInfo[1]['content'] + ", " + schoolInfo[2]['content'] + ", " + schoolInfo[3]['content']
+						schoolListings[name] = {"physicalAddress": schoolAddress, "url": webpage}
+					except UnicodeEncodeError:
+						x = 0
+			pageNumber += 1
+		except requests.exceptions.ConnectionError:
+			x = 0
 def works():
 	global schoolListings
 	global pageNumber
@@ -53,5 +56,6 @@ def works():
 			schoolListings[name] = {"address": ", ".join(schoolAddress)}
 		pageNumber += 1
 		
-works()
+testWorks()
+#print schoolListings
 print json.dumps(schoolListings)
