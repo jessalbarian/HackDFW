70e2f778a80b84003ebc02e87cbd12e504b17775
jessalbarian <albarian.jess@gmail.com>
2016-04-17 00:30:52 -0500

    Added btn and messed with main call function

--- HackDFW2016/HackDFW2016/ViewController.swift
+++ HackDFW2016/HackDFW2016/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let phoneNumber = "7206207466"
+    
+    
+    @IBAction func blueBtn(sender: AnyObject) {
+        callNumber(phoneNumber)
+    }
+    
+    func callNumber(phoneNumber:String){
+//        if let phoneCallURL:NSURL = NSURL(string: "tel://\(phoneNumber)") {
+//            let application:UIApplication = UIApplication.sharedApplication()
+//            if (application.canOpenURL(phoneCallURL)) {
+//            application.openURL(phoneCallURL);
+//            }
+            UIApplication.sharedApplication().openURL(NSURL(string: "telprompt://9809088798")!)
+//        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
